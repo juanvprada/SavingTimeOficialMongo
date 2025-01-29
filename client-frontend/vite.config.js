2f4de38f465daf58a071f63bb9bc99dfedf0012c
juanvprada <juanvprada@gmail.com>
2025-01-29 11:13:51 +0100

    actualizar package de front y de raíz y vite.config.js

--- client-frontend/vite.config.js
+++ client-frontend/vite.config.js
@@ -3,6 +3,15 @@ import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: false,
+    minify: true,
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.json']
+  },
   server: {
     port: 5173,
     host: true,
@@ -19,8 +28,5 @@ export default defineConfig({
         secure: false,
       }
     }
-  },
-  resolve: {
-    extensions: ['.js', '.jsx', '.json']
   }
 })
