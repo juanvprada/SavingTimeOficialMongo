695db9ae53766d94713ad11c74a96929e78ecbe5
juanvprada <juanvprada@gmail.com>
2025-01-29 11:45:44 +0100

    Update API URLs to HTTPS

--- server-backend/src/app.ts
+++ server-backend/src/app.ts
@@ -24,7 +24,7 @@ app.use(cors({
     'http://localhost:5000',
     'http://localhost:5173',
     'http://localhost:8080',
-    'https://tu-dominio-evennode.com'
+    'https://savingtimeoficial.eu-4.evennode.com'
   ],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
